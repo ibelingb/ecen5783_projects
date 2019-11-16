34841aeca666acd33fe4dad4a8a918ca3cc91208
ibelingb <ibelingb@colorado.edu>
2019-11-15 22:07:43 -0700

    Successfully capturing audio via microphone and able to loop recording process and write to mutiple files or repeatedly overwrite existing file.

--- client_pi/microphone.py
+++ client_pi/microphone.py
@@ -5,18 +5,27 @@
 # 11/14/2019
 
 """ microphone.py: 
-                  TODO
 
-    + Resources and Citations +
-    The following resources were used to assist with development of this SW.
-      - 
-      - 
++ Resources and Citations +
+The following resources were used to assist with development of this SW.
+  - https://makersportal.com/blog/2018/8/23/recording-audio-on-the-raspberry-pi-with-python-and-a-usb-microphone
+  - http://wiki.sunfounder.cc/index.php?title=To_use_USB_mini_microphone_on_Raspbian
+  - https://forum.core-electronics.com.au/t/awful-sound-to-noise-ratio-with-mini-usb-microphone/4079
 """
-
 import sys
-import time
+import pyaudio
+import wave
 
 __author__ = "Brian Ibeling"
+
+FORM_1 = pyaudio.paInt16 # 16-bit resolution
+CHANNELS = 1 # 1 channel
+SAMPLE_RATE = 44100 # 48kHz sampling rate
+BUFFER_FRAMES = 4096 # 2^12 samples for buffer
+RECORD_SEC = 3 # seconds to record
+DEVICE_INDEX = 2 # device index found by p.get_device_info_by_index(ii)
+WAV_OUTPUT_FILENAME = 'recordedAudio.wav' # name of .wav file
+NUM_RECORDINGS = 0 # Variable to track number of recordings before ending program
 
 #-----------------------------------------------------------------------
 # Constants
@@ -24,11 +33,55 @@ __author__ = "Brian Ibeling"
 # Object Instances and Variables
 #-----------------------------------------------------------------------
 
+def RecordAudio():
+  audio = pyaudio.PyAudio() # create pyaudio instantiation
+    
+  # create pyaudio stream
+  stream = audio.open(format = FORM_1,rate = SAMPLE_RATE,channels = CHANNELS, \
+                      input_device_index = DEVICE_INDEX,input = True, \
+                      frames_per_buffer=BUFFER_FRAMES)
+
+  print("recording")
+  frames = []
+
+  # loop through stream and append audio chunks to frame array
+  for ii in range(0,int((SAMPLE_RATE/BUFFER_FRAMES)*RECORD_SEC)):
+    data = stream.read(BUFFER_FRAMES, exception_on_overflow = False)
+    frames.append(data)
+
+  print("finished recording")
+
+  # stop the stream, close it, and terminate the pyaudio instantiation
+  stream.stop_stream()
+  stream.close()
+  audio.terminate()
+
+  # save the audio frames as .wav file
+  wavefile = wave.open(WAV_OUTPUT_FILENAME,'wb')
+  wavefile.setnchannels(CHANNELS)
+  wavefile.setsampwidth(audio.get_sample_size(FORM_1))
+  wavefile.setframerate(SAMPLE_RATE)
+  wavefile.writeframes(b''.join(frames))
+  wavefile.close()
+
+  return 0
+
 #-----------------------------------------------------------------------
+""" Main for Client_Pi Microphone process - 
+TOOD
+"""
 def main(args):
-  """ Main for Client_Pi Mircophone process - 
-      TOOD
-  """
+  global NUM_RECORDINGS
+
+  while NUM_RECORDINGS < 5:
+    # Capture audio via microphone, write to .wav file on RPi
+    RecordAudio()
+
+    # Send signal to client_pi process to send audio to AWS 
+    # TODO
+
+    NUM_RECORDINGS += 1
+
   return 0
 
 if __name__ == '__main__':
